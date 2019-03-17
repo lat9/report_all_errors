9b9f7c78ad8ed6687dc7b9702992fc9514975e98
lat9 <lat9@vinosdefrutastropicales.com>
2019-03-17 15:23:13 -0400

    #8: Use zc156a as enable_error_logging.php change basis

--- includes/init_includes/init_report_all_errors.php
+++ includes/init_includes/init_report_all_errors.php
@@ -8,6 +8,6 @@
 ** If the configuration value is set to report all errors during the store's operation, enable it!
 */
 if (defined('REPORT_ALL_ERRORS_STORE') && REPORT_ALL_ERRORS_STORE != 'No') {
-    @ini_set('error_reporting', -1 );
-    set_error_handler('zen_debug_error_handler', -1);
+    @ini_set('error_reporting', E_ALL );
+    set_error_handler('zen_debug_error_handler', E_ALL);
 }
